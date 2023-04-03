b240dd4e7d8a2730cba76e9f49728fa6eedf2f36
Elise Jeandupeux <elise.jeandupeux@gmail.com>
2023-04-03 16:43:01 +0200

    changes

--- build-test.sh
+++ build-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONTROL_LIBRARIES_BRANCH=v7.0.0
 
-IMAGE_NAME=aica-technology/network-interfaces
+IMAGE_NAME=aica-technology/elise-j/net-interfaces
 IMAGE_STAGE=build-test
 
 BUILD_FLAGS=()
